22986454c780d5cd05108e1ed3a97af9c15b14e9
WellingthonSchuh <140724607+WellingthonSchuh@users.noreply.github.com>
2025-11-07 10:56:14 -0300

    add documents

---
+++ Timer.js
@@ -0,0 +1,37 @@
+function timeFromSeconds(segundos){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR',{
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = timeFromSeconds(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function(evento){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+pausar.addEventListener('click', function(evento){
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+zerar.addEventListener('click', function(evento){
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
